c345a4ae872a83bfca753e6469ccbb887caecf5f
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-09-11 22:22:26 +0500

    conversion of type x may be a mistake because neither type overlaps with the other completed with 2 solutions

--- typescript-advanced/tasks/003-conversion-of-type-x-may-be-a-mistake-because-neither-type-overlaps-with-the-other.problem.ts
+++ typescript-advanced/tasks/003-conversion-of-type-x-may-be-a-mistake-because-neither-type-overlaps-with-the-other.problem.ts
@@ -1,7 +1,17 @@
+// 1. Solution by adding bark property into 'cat' object
 interface Dog {
   bark: boolean;
 }
 // it's enough to have 'bark' property in an obeject to be casted into Dog interface
-let cat = { purr: true, bark: false }; // I don't totally understand this question
+let cat = { purr: true, bark: false };
 
 let dog = cat as Dog;
+
+// 2. Solution by using 'unknown' type
+interface AnotherDog {
+  bark: boolean;
+}
+
+let anotherCat = { purr: true };
+
+let anotherDog = anotherCat as unknown as AnotherDog;
