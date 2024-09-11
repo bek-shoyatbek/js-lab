42c9a70824bb5b70b7b82059623f9dae58f76ae3
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-09-11 22:10:04 +0500

    task 003, conversion of type x may be a mistake neither type overlaps with the other

--- typescript-advanced/tasks/003-conversion-of-type-x-may-be-a-mistake-because-neither-type-overlaps-with-the-other.problem.ts
+++ typescript-advanced/tasks/003-conversion-of-type-x-may-be-a-mistake-because-neither-type-overlaps-with-the-other.problem.ts
@@ -1,7 +1,7 @@
 interface Dog {
   bark: boolean;
 }
-
+// it's enough to have 'bark' property in an obeject to be casted into Dog interface
 let cat = { purr: true, bark: false }; // I don't totally understand this question
 
 let dog = cat as Dog;
