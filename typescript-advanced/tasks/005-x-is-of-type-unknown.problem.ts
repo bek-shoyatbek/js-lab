7fa0c289f07602c8bcb5e5bb5f7b6b678ff24d75
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-09-11 22:47:55 +0500

    x is of type unknown completed with 2 solutions

--- typescript-advanced/tasks/005-x-is-of-type-unknown.problem.ts
+++ typescript-advanced/tasks/005-x-is-of-type-unknown.problem.ts
@@ -1,3 +1,4 @@
+// 1. Solution with 'any'
 const somethingDangerous = () => {
   if (Math.random() > 0.5) {
     throw new Error("Oh dear!");
@@ -6,6 +7,19 @@ const somethingDangerous = () => {
 
 try {
   somethingDangerous();
-} catch (error) {
+} catch (error: any) {
   console.log(error.message);
+}
+
+// 2. Solution with type casting
+const anotherSomethingDangerous = () => {
+  if (Math.random() > 0.5) {
+    throw new Error("Oh dear!");
+  }
+};
+
+try {
+  somethingDangerous();
+} catch (error: unknown) {
+  console.log((error as Error).message);
 }
